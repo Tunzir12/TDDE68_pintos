10dfd94e1dec80d97ae90f3baa53eea482902672
jiach613 <jiach613@tlvm-4-4-4.ad.liu.se>
2024-01-16 14:33:42 +0100

    linked list

--- standalone/slist/list.c
+++ standalone/slist/list.c
@@ -1,5 +1,59 @@
 #include "list.h"
-
-
+#include <stdlib.h>
+#include <stdio.h>
 
 /* Implement the functions here */
+void append(struct list_item *first, int x){
+    struct list_item* ptr=first;
+    while(ptr->next!=NULL){
+        ptr=ptr->next;
+    }
+    struct list_item *p = (struct list_item *) malloc ( sizeof (struct  list_item ));
+    p->value=x;
+    p->next=NULL;
+    ptr->next=p;
+}
+void prepend(struct list_item *first, int x){
+    struct list_item* pre=first->next;
+    struct list_item *p = (struct list_item *) malloc ( sizeof (struct  list_item ));
+    p->value=x;
+    p->next=pre;
+    first->next=p;
+}
+//2 4 8 9 
+void input_sorted(struct list_item *first, int x){
+    struct list_item* parent=first;
+    struct list_item* ptr=first->next;
+    while(ptr!=NULL){
+        if(ptr->value>x){
+            break;
+        }
+        parent=ptr;
+        ptr=ptr->next;
+    }
+    struct list_item *p = (struct list_item *) malloc ( sizeof (struct  list_item ));
+    p->value=x;
+    p->next=ptr;
+    parent->next=p;
+
+    
+}
+
+void clear(struct list_item *first){
+    struct list_item* parent=first;
+    struct list_item* ptr=first->next;
+    while(ptr!=NULL){
+        parent=ptr;
+        ptr=ptr->next;
+        free(parent);
+    }
+    first->next=NULL;
+}
+void print(struct list_item *first){
+    struct list_item* ptr=first;
+    while(ptr->next!=NULL){
+        printf("%d ", ptr->next->value);
+        ptr=ptr->next;
+    }
+    printf("\n");
+}
--- standalone/slist/main.c
+++ standalone/slist/main.c
@@ -1,5 +1,5 @@
 #include "list.h"
-
+#include <stdio.h>
 #include <stdlib.h>
 
 int main()
@@ -9,5 +9,24 @@ int main()
 	root.next = NULL;
 
 	/* Write your test cases here */
+	int i;
+	for(i=0;i<10;i++){
+		append(&root,i);
+	}
+	for(i=0;i<3;i++){
+		prepend(&root,i);
+	}
+
+    print(&root);
+	clear(&root);
+	for(i=1;i<10;i+=2){
+		append(&root,i);
+	}
+	print(&root);//1 3 5 7 9
+	input_sorted(&root,0);
+	input_sorted(&root,3);
+	input_sorted(&root,10);
+	print(&root);//0 1 3 3 5 7 9 10
+	clear(&root);
 	return 0;
 }
