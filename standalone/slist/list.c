bfb4279bcc27bc97b4992d8e25f290e6d0740cc8
Maliha Tunzira <maltu866@student.liu.se>
2024-01-20 08:53:58 +0100

    updating list

--- standalone/slist/list.c
+++ standalone/slist/list.c
@@ -1,5 +1,85 @@
 #include "list.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 
 
 /* Implement the functions here */
+
+/* Puts x at the end of the list */
+void append(struct list_item *first, int x){
+
+    while( first->next != NULL ){
+
+        first = first->next;
+    }
+
+    struct list_item *newItem = (struct list_item *)malloc(sizeof(struct list_item));
+
+    newItem->value = x ;
+    newItem->next = NULL;
+
+    first->next = newItem ;
+}
+
+
+/* Puts x at the start of the list */
+void prepend(struct list_item *first, int x){
+
+    struct list_item *newItem = (struct list_item *)malloc(sizeof(struct list_item));
+
+    newItem->value = x ;
+    newItem->next = first->next;
+    first->next = newItem;
+}
+
+
+/* Find the first element in the list larger than x and
+ * put x right before that element */
+void input_sorted(struct list_item *first, int x){
+
+    struct list_item *current = first;
+    struct list_item *temp = first->next;
+
+    while( temp != NULL ){
+
+        if(temp->value>x){
+            break;
+        }
+        current = temp;
+        temp = temp->next;
+    }
+
+    struct list_item *newItem = (struct list_item *)malloc(sizeof(struct list_item));
+
+    newItem->value = x ;
+    newItem->next = temp;
+    current->next = newItem;
+}
+
+void print(struct list_item * first){
+
+    if(first->next == NULL && first->value <= -1){
+        printf("List is empty");
+    }
+
+    while (first->next != NULL)
+    {
+        printf("%d ",first->next->value);
+        first = first->next;
+    }
+    printf("\n");
+
+}
+
+void clear(struct list_item *first){
+
+    struct list_item* current = first;
+    struct list_item* temp= first->next;
+    while( temp != NULL){
+        current = temp;
+        temp = temp->next;
+        free(current);
+    }
+    first->next = NULL;
+}
--- standalone/slist/main.c
+++ standalone/slist/main.c
@@ -1,5 +1,5 @@
 #include "list.h"
-
+#include<stdio.h>
 #include <stdlib.h>
 
 int main()
@@ -9,5 +9,22 @@ int main()
 	root.next = NULL;
 
 	/* Write your test cases here */
+
+	append(&root, 3);
+
+	print(&root);
+	append(&root, 2);
+	prepend(&root, 5);
+	input_sorted(&root, 4);
+	input_sorted(&root, 7);
+	print(&root);
+	
+	clear(&root);
+
+	append(&root, 9);
+
+	print(&root);
+	
+
 	return 0;
 }
